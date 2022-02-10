a51f80081967587e56e933544b73319e35cdb887
Ungedummt <Ungedummt@users.noreply.github.com>
2022-02-10 08:37:09 +0000

    deploy: 8f08e7284ac43aaf07ab500f7e215f7ed69300fb

--- git/docs/gsk4/prelude/sidebar-items.js
+++ git/docs/gsk4/prelude/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["GskRenderer","Trait containing all [`struct@Renderer`] methods."],["GskRendererExtManual",""]]});
+initSidebarItems({"trait":[["GskRendererExt","Trait containing all [`struct@Renderer`] methods."],["IsRenderNode","A common trait implemented by the various `RenderNode` types."]]});
